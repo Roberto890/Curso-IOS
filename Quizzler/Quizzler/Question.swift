13f928e40dd0b200ada6974f4d838d15dbe02b18
Roberto890 <roberto.jesus.amaral@gmail.com>
2021-09-14 15:18:02 -0300

    initial commit - Quizzler

---
+++ Quizzler/Quizzler/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Virtual Machine on 14/09/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+    
+}
