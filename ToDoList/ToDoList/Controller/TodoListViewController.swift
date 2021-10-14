a59a7b3c33102ea90357f8ce840ca93ba8310d6d
Mac Solutis <sem.email@email.com>
2021-10-14 11:37:46 -0700

    ToDoList - initial commit

---
+++ ToDoList/ToDoList/Controller/TodoListViewController.swift
@@ -0,0 +1,115 @@
+//
+//  TodoListViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    var itemArray = [ItemModel]()
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loadItems()
+        
+    }
+    
+    //MARK: Add new items
+    
+    @IBAction func AddButtonPress(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // what will happen when click in add item
+            
+            let newItem = ItemModel()
+            newItem.name = textField.text!
+            if newItem.name != "" {
+                self.itemArray.append(newItem)
+                
+                self.saveItems()
+                
+            }
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    //MARK: TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.name
+        
+        //if item.checked is true make a checkmark else dismark
+        cell.accessoryType = item.checked ? .checkmark : .none
+        
+        return cell
+        
+    }
+
+    //MARK: tableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        itemArray[indexPath.row].checked = !itemArray[indexPath.row].checked
+
+        saveItems()
+            
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK: -Model Manipulation Methods
+    func saveItems(){
+        
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([ItemModel].self, from: data)
+            } catch {
+                print("Error decodint item array, \(error)")
+            }
+        }
+    }
+
+}
+
---
+++ ToDoList/ToDoList/Data Model/ItemModel.swift
@@ -0,0 +1,14 @@
+//
+//  ItemModel.swift
+//  ToDoList
+//
+//  Created by Administrator on 10/14/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class ItemModel: Codable {
+    var name: String = ""
+    var checked: Bool = false
+}
